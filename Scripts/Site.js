e5f07b4a4a028dd61ee5a768b0333fe4ceb5b5ef
陈志同 <chenzhitong12@qq.com>
2017-03-02 23:18:06 +0800

    完善账户管理的个人资料页面

---
+++ Scripts/Site.js
@@ -0,0 +1,22 @@
+﻿function ready_to_edit(obj)
+{
+    $(obj).parent().removeClass("placeholder").removeClass("finished").addClass("edit");
+    $(obj).parent().find(".form-control").focus();
+}
+
+function finish_to_edit(obj)
+{
+    var _default = "*********";
+    if ($(obj).val().length <= 0)
+    {
+        $(obj).parent().removeClass("finished").removeClass("edit").addClass("placeholder");
+        $(obj).parent().find("label").text(_default);
+        return;
+    }
+    $(obj).parent().removeClass("placeholder").removeClass("edit").addClass("finished");
+    $(obj).parent().find("label").text($(obj).val());
+}
+$(function ()
+{
+    $('[data-toggle="tooltip"]').tooltip()
+})
